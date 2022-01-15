e7dfd9e70a4edd0f4777eb78dd0d426d13f62d66
mattcroat <mattcroat.dev@gmail.com>
2022-01-15 18:54:21 +0100

    feat: Timer

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,10 +1,13 @@
+import { useEffect, useRef, useState } from 'react'
 import { HeadersFunction, LoaderFunction, useLoaderData } from 'remix'
 import { getPost, PostMarkdown } from '~/post'
+
+let seconds = 60
 
 export let headers: HeadersFunction = () => {
   return {
     'X-Gon-Give-It-To-Ya': 'DMX',
-    'Cache-Control': 'max-age=300, s-maxage=3600',
+    'Cache-Control': `max-age=0, s-maxage=${seconds}`,
   }
 }
 
@@ -13,10 +16,20 @@ export let loader: LoaderFunction = async ({ params }) => {
 }
 
 export default function PostSlug() {
+  let [duration, setDuration] = useState(seconds)
   let post = useLoaderData<PostMarkdown>()
+
+  useEffect(() => {
+    if (duration === 0) return
+    let interval = setInterval(() => setDuration(duration - 1), 1000)
+    return () => clearInterval(interval)
+  }, [duration])
 
   return (
     <div>
+      <p style={{ color: 'tomato' }}>
+        This page is valid for {duration} seconds
+      </p>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
     </div>
   )
